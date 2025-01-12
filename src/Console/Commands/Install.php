b6950fb7df2d9ffd69ca4610c86eb98d6395a227
opoink <ronromeperez@yahoo.com>
2025-01-12 22:51:43 +0800

    add the Plugin namespace to autoload upon install, and add the env need env variables

--- src/Console/Commands/Install.php
+++ src/Console/Commands/Install.php
@@ -32,6 +32,8 @@ class Install extends Command
 		$resouceDir = base_path('vendor'.$ds.'opoink'.$ds.'oliv'.$ds.'src'.$ds.'resources');
 
 		try {
+			$this->addPluginsToComposer();
+
 			/** copy the plugins dir */
 			$this->info('Copying the plugins directory...');
 			$dirManager->copyDir($resouceDir.$ds.'plugins', base_path().$ds.'plugins');
@@ -41,8 +43,9 @@ class Install extends Command
 			$this->info('Copying the routes directory...');
 			$dirManager->copyDir($resouceDir.$ds.'routes', base_path().$ds.'routes');
 			$this->info('Routes directory copied successfully.');
-	
 
+			$this->addValueToEnv('VITE_ADMIN_URL', '"admin_abc123"');
+			$this->addValueToEnv('VITE_INERTIA_SSR_PORT', '13714');
 	
 	
 	
@@ -51,6 +54,44 @@ class Install extends Command
 		} catch (\Exception $e) {
 			$this->error($e->getMessage());
 		}
+	}
+
+	protected function addPluginsToComposer(){
+		$this->info('Adding plugins to composer autoload...');
+		$composer = json_decode(file_get_contents(base_path('composer.json')), true);
+		$composer['autoload']['psr-4']['Plugins\\'] = 'plugins/';
+		file_put_contents(base_path('composer.json'), json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+
+		$this->info('Running composer dump-autoload...');
+		exec('composer dump-autoload');
+
+		$this->info('Plugins added to composer autoload successfully.');
+	}
+
+	protected function addValueToEnv($key, $value){
+		$this->info('Adding '.$key.' to .env file...');
+		$envFile = base_path('.env');
+		$env = file_get_contents($envFile);
+
+		$env = explode("\n", $env);
+
+		$founded = false;
+		
+		foreach ($env as $k => $v) {
+			$keyVal = explode('=', $v);
+			if(isset($keyVal[0]) && $keyVal[0] == $key){
+				$keyVal[1] = $value;
+				$founded = true;
+			}
+
+			$newEnv[] = implode('=', $keyVal);
+		}
+
+		if(!$founded){
+			$newEnv[] = $key.'='.$value;
+		}
+		
+		file_put_contents($envFile, implode("\n", $newEnv));
 	}
 
 
