03d70a07e21cd950d063feaf708ceee50e7cfbed
opoink <37372895+opoink@users.noreply.github.com>
2025-06-27 15:58:59 +0800

    change the admin menu to use name as identifier of the menu. and create a merger class to merge the menu

--- src/resources/plugins/Opoink/Cms/config/adminmenu.php
+++ src/resources/plugins/Opoink/Cms/config/adminmenu.php
@@ -1,23 +1,27 @@
 <?php
 return [
-	'cms' => [
+	[
+		'name' => 'cms',
 		'route' => null,
 		'label' => 'CMS',
 		'fa_icon' => 'fa-solid fa-file-code',
 		'is_active_menu_url' => '/cms',
 		'is_active_menu_name' => 'cms',
 		'role_resource' => 'cms',
-		'child' => [
-			'col_0' => [
-				'content' => [
+		'children' => [
+			[
+				[
+					'name' => 'cms.content',
 					'title' => 'Content',
 					'links' => [
 						[
+							'name' => 'cms.content.block',
 							'route' => '/cms/block',
 							'label' => 'Blocks',
 							'role_resource' => 'cms_block'
 						],
 						[
+							'name' => 'cms.content.pages',
 							'route' => '/cms/pages',
 							'label' => 'Pages',
 							'role_resource' => 'cms_pages'
