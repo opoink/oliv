6959e61625d5a000dc9d0460f65cc5cc9fce622f
opoink <37372895+opoink@users.noreply.github.com>
2025-07-04 14:10:14 +0800

    add parent tabs template and css

--- src/resources/plugins/Opoink/Liv/resources/js/States/admin.side.tabs.js
+++ src/resources/plugins/Opoink/Liv/resources/js/States/admin.side.tabs.js
@@ -1,58 +1,26 @@
 import { reactive } from 'vue';
-import { router } from '@inertiajs/vue3';
 
 export class AdminSideTabs {
 
 	isActiveTab = '';
-	// queryParam = 'active-tab';
-	// url_params = null;
-
-	constructor(){
-		// if(typeof window != 'undefined'){
-		// 	this.url_params = new URLSearchParams(window.location.search);
-		// }
-	}
-
-	// getParam(key){
-	// 	let val = null;
-	// 	if(this.url_params){
-	// 		val = this.url_params.get(this.queryParam);
-	// 	}
-	// 	return val;
-	// }
-
-	// setDefaultTab(defaultTab){
-	// 	this.defaultTab = defaultTab;
-	// 	return this;
-	// }
-
-	// setQueryParam(queryParam){
-	// 	this.queryParam = queryParam;
-	// 	return this;
-	// }
-
-	// isActiveTab(value){
-	// 	let inUrlActiveTab = this.getParam();
-		
-	// 	console.log('isActiveTab isActiveTab', inUrlActiveTab);
-	// 	if(inUrlActiveTab){
-	// 		return inUrlActiveTab == value;
-	// 	}
-	// 	else {
-	// 		return this.defaultTab == value;
-	// 	}
-	// }
+	activeParents = {};
 
 	setActiveTab(value){
 		this.isActiveTab = value;
-		// this.url_params.set(this.queryParam, value);
-		// console.log(this.url_params);
-		// if(typeof route.params[this.queryParam] != 'undefined') {
-		// 	return route.params[this.queryParam] = value;
-		// }
-		// else{
-		// 	return this.defaultTab = value;
-		// }
+	}
+
+	setActiveParent(key){
+		if(typeof this.activeParents[key] == 'undefined') {
+			this.activeParents[key] = true;
+		}
+		else {
+			if(this.activeParents[key] == true){
+				this.activeParents[key] = false;
+			}
+			else {
+				this.activeParents[key] = true;
+			}
+		}
 	}
 }
 
