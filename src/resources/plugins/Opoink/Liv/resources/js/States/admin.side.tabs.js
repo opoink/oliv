f6bcbbfb51d32e3eb468b9760ddc57261ef05af9
opoink <37372895+opoink@users.noreply.github.com>
2025-07-04 17:07:08 +0800

    add reset method

--- src/resources/plugins/Opoink/Liv/resources/js/States/admin.side.tabs.js
+++ src/resources/plugins/Opoink/Liv/resources/js/States/admin.side.tabs.js
@@ -1,14 +1,22 @@
 import { reactive } from 'vue';
+import { getAdminUrl, base64EncodeUrl } from '@@Plugins@@/Opoink/Liv/resources/js/Lib/common';
 
 export class AdminSideTabs {
 
 	isActiveTab = '';
 	activeParents = {};
 
-	setActiveTab(value){
-		this.isActiveTab = value;
+	/**
+	 * @param {*} key string identifier of an active tab
+	 */
+	setActiveTab(key){
+		this.isActiveTab = key;
 	}
 
+	/**
+	 * 
+	 * @param {*} key string identifier of the parent active tab
+	 */
 	setActiveParent(key){
 		if(typeof this.activeParents[key] == 'undefined') {
 			this.activeParents[key] = true;
@@ -21,6 +29,27 @@ export class AdminSideTabs {
 				this.activeParents[key] = true;
 			}
 		}
+	}
+
+	getAdminUrl(path){
+		let params = {
+			active_tab: this.isActiveTab,
+			active_parents: ''
+		};
+
+		let activeParents = [];
+		Object.keys(this.activeParents).forEach((val) => {
+			if(this.activeParents[val]) {
+				activeParents.push(val);
+			}
+		});
+		params.active_parents = base64EncodeUrl(activeParents.join(','));
+		return getAdminUrl(path, params);
+	}
+	
+	reset(){
+		this.isActiveTab = '';
+		this.activeParents = {};
 	}
 }
 
