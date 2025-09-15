45f3b546fb977efbfcbdfc3a8bee258a3fc6dcae
opoink <37372895+opoink@users.noreply.github.com>
2025-09-15 11:38:08 +0800

    add empty value to filter field select

--- src/resources/plugins/Opoink/Liv/resources/js/Components/Admin/ListingFilter/FilterFieldSelect.vue
+++ src/resources/plugins/Opoink/Liv/resources/js/Components/Admin/ListingFilter/FilterFieldSelect.vue
@@ -27,6 +27,7 @@
 		<div class="mb-3">
 			<label :for="'filter-fields-' + field" class="form-label fw-bold mb-0">{{name}}</label>
 			<select class="form-select" :aria-label="field" :id="'filter-fields-' + field" :name="field" v-model="fieldModel" @change="handleChange()">
+				<option value="">Please Select</option>
 				<option v-for="option in options" :value="option.value">{{ option.label }}</option>
 			</select>
 		</div>
