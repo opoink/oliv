b2f4894ec44c325d81554d3e28b09b732d23e2eb
opoink <37372895+opoink@users.noreply.github.com>
2025-09-15 12:49:30 +0800

    add label text to active filter

--- src/resources/plugins/Opoink/Liv/resources/js/Components/Admin/ListingFilter/ActiveFilterFieldRange.vue
+++ src/resources/plugins/Opoink/Liv/resources/js/Components/Admin/ListingFilter/ActiveFilterFieldRange.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
-	import { ref } from 'vue';
+	import { ref, onBeforeMount } from 'vue';
 
 	const emit = defineEmits(['removeFilter']);
 
 	const props = defineProps([
 		'listingFilter', 
-		'field'
+		'field',
+		'label'
 	]);
 
 	const lf = ref(props.listingFilter);
 
 	const from = ref(props.listingFilter.filterFields[props.field].from);
 	const to = ref(props.listingFilter.filterFields[props.field].to);
+
+	const label = ref(props.label);
+
+	onBeforeMount(() => {
+		if(!label.value){
+			label.value = 'Column Name';
+		}
+	});
 
 	const removeFilter = function(){
 		lf.value.filterFields[props.field].from = "";
@@ -24,7 +33,7 @@
 
 <template>
 	<span class="d-block py-1" v-if="from || to">
-		<span class="text-muted">ID: </span>{{from}} - {{to}}
+		<span class="text-muted">{{label}}: </span>{{from}} - {{to}}
 		<span class="ms-2 c-pointer">
 			<i class="fa-solid fa-times-circle" @click="removeFilter();"></i>
 		</span>
--- src/resources/plugins/Opoink/Liv/resources/js/Components/Admin/ListingFilter/ActiveFilterFieldText.vue
+++ src/resources/plugins/Opoink/Liv/resources/js/Components/Admin/ListingFilter/ActiveFilterFieldText.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
-	import { ref } from 'vue';
+	import { ref, onBeforeMount } from 'vue';
 
 	const emit = defineEmits(['removeFilter']);
 
 	const props = defineProps([
 		'listingFilter', 
-		'field'
+		'field',
+		'label'
 	]);
 
 	const lf = ref(props.listingFilter);
+
+	const label = ref(props.label);
+
+	onBeforeMount(() => {
+		if(!label.value){
+			label.value = 'Column Name';
+		}
+	});
 
 	const removeFilter = function(){
 		lf.value.filterFields[props.field] = "";
@@ -20,7 +29,7 @@
 
 <template>
 	<span class="d-block py-1" v-if="lf.filterFields[field]">
-		<span class="text-muted">Role: </span>{{lf.filterFields[field]}}
+		<span class="text-muted">{{ label }}: </span>{{lf.filterFields[field]}}
 		<span class="ms-2 c-pointer">
 			<i class="fa-solid fa-times-circle" @click="removeFilter()"></i>
 		</span>
